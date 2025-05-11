c365d093dee48757fb2bfe056254ade077207834
胡金武 <sliverm@outlook.com>
2025-05-11 11:09:24 +0800

    feat: change origin

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const TIMEOUT = 1000000;
-const API_BASE_URL = "https://djw3crnmuc.ap-southeast-2.awsapprunner.com";
+const API_BASE_URL = "https://pryevz3dwx.ap-southeast-2.awsapprunner.com";
 
 export function postmessage(message: string, randomNumber: number) {
     return axios.post(`${API_BASE_URL}/llm_talk`, {
